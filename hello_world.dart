6e7d745ee73ab25b9121c483614356551850064b
merkuree <merkuree@naver.com>
2023-03-30 11:22:12 +0900

    map and set type

--- hello_world.dart
+++ hello_world.dart
@@ -1,8 +1,23 @@
-void main() {
+void hello() {
   //nullable이 가능하도록
   String? nico = 'nico';
-  nico = null;
+  nico = "";
+  print(nico?.isEmpty);
   print(isEmpty(nico));
+  var age = 0;
+  age = 33;
+  print(age);
+  final name = 'nico';
+  print(name);
+  print('name');
+  late final String name2;
+  name2 = 'nico2';
+
+  const API = '12121221212';
+  num abc = 3;
+  abc = 3.3;
+  bool abcd = true;
+  double abcde = 3.3;
 }
 
 isEmpty(String? string) => string?.length == 0 ? 'aaa' : string;
---
+++ main.dart
@@ -0,0 +1,9 @@
+// import 'hello_world.dart';
+// import 'list_type.dart';
+import 'map.dart';
+
+void main() {
+  // hello();
+  // list_type();
+  map_type();
+}
