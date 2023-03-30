e4a7584a648a80bfcf77e836d9c666d94b4f4810
merkuree <merkuree@naver.com>
2023-03-30 15:13:21 +0900

    optional parameter

--- main.dart
+++ main.dart
@@ -1,11 +1,14 @@
 // import 'hello_world.dart';
 // import 'list_type.dart';
-import 'functions.dart';
+// import 'functions.dart';
 // import 'map.dart';
+
+import 'optional_param.dart';
 
 void main() {
   // hello();
   // list_type();
   // map_type();
-  func();
+  // func();
+  optional_param();
 }
