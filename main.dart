f3b524b1239bff50a5e00e7838cd0f7fe1ff5b1b
merkuree <merkuree@naver.com>
2023-03-30 15:01:32 +0900

    function parameter

--- main.dart
+++ main.dart
@@ -1,9 +1,11 @@
 // import 'hello_world.dart';
 // import 'list_type.dart';
-import 'map.dart';
+import 'functions.dart';
+// import 'map.dart';
 
 void main() {
   // hello();
   // list_type();
-  map_type();
+  // map_type();
+  func();
 }
