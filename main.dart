35abb9659066759feb3a9931b2708eff378ecf66
merkuree <merkuree@naver.com>
2023-03-31 10:52:21 +0900

    상속관련 study

--- main.dart
+++ main.dart
@@ -1,34 +1,28 @@
-typedef PlayerList = List<Map<String, dynamic>>;
-
 enum Team {red, blue}
-enum XPLevel {beginner, medium, pro}
 
-class Player {
-  String name;
-  Team team;
-  XPLevel xp;
-  int age;
-
-  Player({required this.name, required this.xp, required this.team, this.age = 20});
-
-  Player.fromJson(Map<String, dynamic> playerJson) :
-    name = playerJson['name'],
-    xp = playerJson['xp'],
-    age = playerJson['age'],
-    team = playerJson['team'];
-
+class Human {
+  final String name;
+  Human(this.name);
   void sayHello() {
-    print("Hi my name is ${name}");
+    print("Hi! My name is $name");
+  }
+}
+
+class Player extends Human {
+  final Team team;
+  Player({
+    required this.team,
+    required String name
+  }) :super(name);
+
+  @override
+  void sayHello() {
+    // TODO: implement sayHello
+    print("Hi! My name is ${super.name}. and My Team is ${team} ");
   }
 }
 
 void main() {
-  var nico = Player(name:'nico', xp: XPLevel.beginner, age: 33, team:Team.red);
-  var lynn = nico
-  ..name = 'las'
-  ..xp = XPLevel.pro
-  ..team = Team.blue
-  ..age = 23
-  ..sayHello();
-  print(lynn.team.toString());
+  var player = Player(team:Team.blue, name:'nico');
+  player.sayHello();
 }
