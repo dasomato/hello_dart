8d9051f3092e0c88c69b88c7657f689b4b5374cc
merkuree <merkuree@naver.com>
2023-03-31 10:57:42 +0900

    mixin study

--- main.dart
+++ main.dart
@@ -1,3 +1,13 @@
+class Strong {
+  final double strengthLevel = 1500.09;
+}
+
+class QuickRunner {
+  void runQuick() {
+    print("runnnnnnnnn!");
+  }
+}
+
 enum Team {red, blue}
 
 class Human {
@@ -8,21 +18,29 @@ class Human {
   }
 }
 
-class Player extends Human {
+class Tall {
+  final double height = 1.99;
+}
+
+class Kid with QuickRunner {}
+
+class Player with Strong, QuickRunner, Tall {
   final Team team;
   Player({
     required this.team,
     required String name
-  }) :super(name);
+  });
 
   @override
   void sayHello() {
     // TODO: implement sayHello
-    print("Hi! My name is ${super.name}. and My Team is ${team} ");
+    print("Hi! My name is . and My Team is ${team} ");
   }
 }
 
 void main() {
   var player = Player(team:Team.blue, name:'nico');
   player.sayHello();
+  var kid = Kid();
+  kid.runQuick();
 }
